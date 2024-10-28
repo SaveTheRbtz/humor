e4e11ff9a58ce07ef0453c683d8d42e52ae61d01
Alexey Ivanov <rbtz@ph34r.me>
2024-10-27 20:57:26 -0700

    web: add footer github links

--- web/src/Footer.jsx
+++ web/src/Footer.jsx
@@ -1,4 +1,4 @@
-import { FaTwitter } from 'react-icons/fa';
+import { FaTwitter, FaGithub } from 'react-icons/fa';
 import './Footer.css';
 
 function Footer() {
@@ -21,6 +21,24 @@ function Footer() {
       >
         <FaTwitter size={24} style={{ marginRight: '8px', marginLeft: '8px' }} />
         <b>@SaveTheRbtz</b>
+      </a>
+      <a
+        href="https://github.com/altsoph/humor-mechanics"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="footer-link"
+      >
+        <FaGithub size={24} style={{ marginRight: '8px', marginLeft: '8px' }} />
+        <b>humor-mechanics</b>
+      </a>
+      <a
+        href="https://github.com/SaveTheRbtz/humor"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="footer-link"
+      >
+        <FaGithub size={24} style={{ marginRight: '8px', marginLeft: '8px' }} />
+        <b>humor-arena</b>
       </a>
     </footer>
   );
